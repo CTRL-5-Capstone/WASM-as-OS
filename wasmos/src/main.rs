6ae83f326b4ae542c708200d4f9355b1e6495984
benjiw02 <150188804+benjiw02@users.noreply.github.com>
2025-10-18 00:11:35 -0500

    Added utilities folder with functions and menus for storing .wasm files. add file for holding paths to wasm files

--- wasmos/src/main.rs
+++ wasmos/src/main.rs
@@ -1,10 +1,15 @@
+//Dependacies
 use dialoguer::{Select, theme::ColorfulTheme};
+use clearscreen;
+//Modules
+mod utilities;
 fn main() {
-    let menu: Vec<_> = vec![
+    let mainmenu: Vec<_> = vec![
         "Load .Wasm File",
+        "Remove .Wasm File",
         "Runtime Metrics",
-        "Start Wasm",
-        "Halt Wasm",
+        "Start wasm",
+        "Stop wasm",
         "Prioritize Wasm's",
         "Save Machine State",
         "Shutdown",
@@ -13,24 +18,22 @@ fn main() {
     loop {
         let choice = Select::with_theme(&ColorfulTheme::default())
             .with_prompt("WasmAsOS")
-            .items(&menu)
+            .items(&mainmenu)
             .default(0)
             .interact()
             .unwrap();
 
         match choice {
-            0 => println!("Load Wasm Code"),
-            1 => println!("Display Runtime Metrics"),
-            2 => println!("Start a Wasm file"),
-            3 => println!("Halt Wasm file"),
-            4 => println!("Diplay Menu for prioritizing recources"),
-            5 => println!("Store the current state to a file"),
-            6 => {
-                println!("End Loop");
-                break;
-            }
+            0 => utilities::wasm_loader::load_menu(),
+            1 => println!("Delete.wasm Menu"),
+            2 => println!("Display Runtime Metrics"),
+            3 => println!("Start a Wasm file"),
+            4 => println!("Halt Wasm file"),
+            5 => println!("Diplay Menu for prioritizing recources"),
+            6 => println!("Store the current state to a file"),
+            7 => break,
             _ => unreachable!(),
         }
-        println!(); // Add a newline for spacing
+        //clearscreen::clear().expect("ERROR Clearing Screen");
     }
 }
