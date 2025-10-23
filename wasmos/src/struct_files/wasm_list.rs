ea1232f10b532a75887a44ba7f8345c6e86c10f3
benjiw02 <150188804+benjiw02@users.noreply.github.com>
2025-10-23 00:47:47 -0500

    File manipulation implemented: wasm file data stored and fetched from wasm_list.csv. Need to fix a few bugs and add reading in from the file at the start of the program.

--- wasmos/src/struct_files/wasm_list.rs
+++ wasmos/src/struct_files/wasm_list.rs
@@ -1,9 +1,66 @@
 use dialoguer::{Select, theme::ColorfulTheme};
 use super::wasm_struct::WasmFile;
+use std::fs;
+
+pub fn delete_from_file(count: u8)
+{
+    let mut newlines: u8 = 0;
+    let mut to_file = String::new();
+    let from_file = fs::read_to_string( "./src/wasm_files/wasm_list.csv").expect("ERROR: Path to wasm_list.csv not found");
+    for itter in from_file.chars()
+    {
+        if newlines != count
+        {
+                to_file.push(itter);           
+        }  
+        if itter == '\n'
+        {
+            newlines += 1;
+        } 
+    }
+    fs::write("./src/wasm_files/wasm_list.csv", to_file).expect("ERROR: Path to wasm_list.csv not found");
+}
+fn add_to_file(count: u8, to_insert: String)
+{
+    let mut lines: u8 = 0;
+    let from_file: String = fs::read_to_string("./src/wasm_files/wasm_list.csv").expect("ERROR: Path to wasm_list.csv not found");
+    let mut to_file =  String::new();
+    let mut before_file = String::new();
+    if from_file.is_empty()
+    {
+        to_file = to_insert;
+    }
+    else
+    {
+        for itter in from_file.chars()
+        {
+            if lines == count
+            {
+                to_file = format!("{}{}", before_file, to_insert);
+                lines += 1;
+            }
+            if lines < count
+            {
+                before_file.push(itter);
+            }
+            else
+            {
+                to_file.push(itter);
+            }
+            if itter == '\n'
+            {
+                lines += 1
+            }
+        }
+    }
+    fs::write("./src/wasm_files/wasm_list.csv", to_file).expect("ERROR: Path to wasm_list.csv not found");
+}
 pub struct WasmList
 { //Stores a Structure WasmFile which holds information about .wasm files.
-    head: Option<Box<WasmNode>>
+    head: Option<Box<WasmNode>>,
+    tail: Option<Box<WasmNode>>
 }
+#[derive(Clone)]
 struct WasmNode
 { //Node for WasmList
     wasm_file: WasmFile, //.wasm file structure
@@ -26,17 +83,26 @@ impl WasmList
     {
         WasmList
         {
-            head: None
+            head: None,
+            tail: None
         }
     }
     pub fn insert(&mut self, node: WasmFile) //Node Insertion Function. Inserts files alphabetically
     {   
         let menu: Vec<_> = vec!["Yes","No"]; //Menu
+        let mut count: u8 = 0;
+        let to_insert = format!("{}, {}\n", node.name, node.path_to); //To be inserted into wasm_list.csv
 
-        if self.head.is_none() || node.name < self.head.as_ref().unwrap().wasm_file.name //Case for no head or new nodes before head
+        if self.head.is_none() || self.head.as_ref().unwrap().wasm_file.name > node.name //Case for no head or new nodes before head
         {
-            let new_node = Box::new(WasmNode::new_node(node, self.head.take()));
+            let new_node = Box::new(WasmNode::new_node(node, self.head.clone()));
             self.head = Some(new_node);
+            if self.head.as_mut().unwrap().next.is_none()
+            {
+                self.tail = self.head.clone();
+            }
+            add_to_file(count, to_insert);
+            self.print();
         }
         else if self.head.as_ref().unwrap().wasm_file.name == node.name //Case for node equal to head
         {
@@ -50,6 +116,8 @@ impl WasmList
                 0 => 
                 {
                     self.head.as_mut().unwrap().wasm_file = node;
+                    delete_from_file(count);
+                    add_to_file(count, to_insert);
                 },
                 1 => (),
                 _ => unreachable!(),
@@ -60,38 +128,51 @@ impl WasmList
             let mut current = self.head.as_mut().unwrap();
             while current.next.is_some() //Iterate thorugh list until spot found where new node name is less than the next node
             {
+                count += 1;
                 let next = current.next.as_mut().unwrap();
-                if next.wasm_file.name > node.name //Insert Node
+                if next.wasm_file.name >= node.name //Insert Node
                 {
-                    let new_node = Box::new(WasmNode::new_node(node, current.next.take()));
-                    current.next = Some(new_node);
-                    return;
-                }
-                else if next.wasm_file.name == node.name //If new node name is already in list ask to replace
-                {
-                    let choice = Select::with_theme(&ColorfulTheme::default())
-                        .with_prompt("File with that name already exists, replace it?")
-                        .items(&menu)
-                        .default(0)
-                        .interact()
-                        .unwrap();
-                    match choice {
-                        0 => 
-                        {
-                            next.wasm_file = node;
-                            return
-                        },
-                        1 => return,
-                        _ => unreachable!(),
-                    }
+                    break;
                 }
                 current = current.next.as_mut().unwrap(); //Traverse node
             }
-            current.next = Some(Box::new(WasmNode::new_node(node, None))); //Create the new node at end of the list
+            if current.next.is_none()
+            {
+                current.next = Some(Box::new(WasmNode::new_node(node, None))); //Create the new node at end of the list
+                self.tail = current.next.clone();
+                add_to_file(count, to_insert);
+            }
+            else if current.next.as_ref().unwrap().wasm_file.name == node.name //If new node name is already in list ask to replace
+            {
+                let choice = Select::with_theme(&ColorfulTheme::default())
+                    .with_prompt("File with that name already exists, replace it?")
+                    .items(&menu)
+                    .default(0)
+                    .interact()
+                    .unwrap();
+                match choice {
+                    0 => {
+                        current.next.as_mut().unwrap().wasm_file = node;
+                        delete_from_file(count);
+                        add_to_file(count, to_insert);
+                    }
+                    1 => (),
+                    _ => unreachable!(),
+                }
+            }
+            else 
+            {
+                    let new_node = Box::new(WasmNode::new_node(node, current.next.take()));
+                    current.next = Some(new_node); 
+                    add_to_file(count, to_insert);
+            }
         }
+
+
     }
     pub fn delete(&mut self, name: String) //Function for removing the node from the list
     {
+        let mut count: u8 = 0;
         let menu: Vec<_> = vec!["Yes","No"];
         let choice = Select::with_theme(&ColorfulTheme::default())
             .with_prompt("Delete File?")
@@ -119,6 +200,7 @@ impl WasmList
             let mut current = self.head.as_mut().unwrap(); //Set Traversing node to head.
             loop 
             {
+                count += 1;
                 if current.next.is_none() //Case for node not found
                 {
                     println!("\nData Corruption, File Not Found\n");
@@ -135,7 +217,18 @@ impl WasmList
             let next = current.next.as_mut().unwrap();
             let new_next = next.next.take();
             current.next = new_next;
+            
         }
+        delete_from_file(count);
+    }
+    pub fn print(&mut self)
+    {
+        let mut current = self.head.as_ref();
+        while let Some(node) = current
+        {
+            println!("{}", node.wasm_file.name);
+            current = node.next.as_ref();
 
+        }
     }
 }
